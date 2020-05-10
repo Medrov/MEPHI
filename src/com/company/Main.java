68b3b8786de8cf3fd9d03f34dae17bb245a9af9f
Mederov <eislo@yandex.ru>
2020-05-10 15:23:33 +0300

    Добавление комментариев

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,51 +1,61 @@
 package com.company;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
-
+// Здрствуйте, Дмитрий Андреевич, Специально для вас сделал коменнтарии
 public class Main {
-    Scanner scanner = new Scanner(System.in);
+    //Обьявлиния листа которые будет показывать табоицу
     private static List<List<String>> table;
-    private static String[] arrayFileStudent;
+    //Массив String для определиния строк в файле с оценками
     private static String[] arrayFileMarks;
-    private static String[] arrayFileSubject;
+    // Это две мапы для получение оценок
     private static Map<Integer, Integer> nameAndSubject1;
     private static Map<Integer, Integer> nameAndSubject2;
+    //Это мапа для хранения имен
     private static Map<Integer, String> nameAndNumber;
+    //Это мапа для хранения предметов
     private static Map<Integer, String> subjectAndNumber;
 
     public static void main(String[] args) throws IOException {
+        //Три стринги путей к файлам, для проверки можете указать свои пути
         String studentPath = "C:\\Users\\eislo\\OneDrive\\Desktop\\Final Work\\student.txt";
         String marksPath = "C:\\Users\\eislo\\OneDrive\\Desktop\\Final Work\\marks.txt";
         String subjectPath = "C:\\Users\\eislo\\OneDrive\\Desktop\\Final Work\\subject.txt";
+        //Три BufferedReader, чтобы считать текст с файлов
         BufferedReader inStudent = new BufferedReader(new FileReader(studentPath));
         BufferedReader inMarks = new BufferedReader(new FileReader(marksPath));
         BufferedReader inSubject = new BufferedReader(new FileReader(subjectPath));
+        //Три болваники
         String strForStudent, strForMarks, StrForSubject;
+        //Три list для хранения файлов
         List<String> listStudent = new ArrayList<>();
         List<String> listMarks = new ArrayList<>();
         List<String> listSubject = new ArrayList<>();
+        //Получинеи лайнов
         while((strForStudent = inStudent.readLine()) != null)
             listStudent.add(strForStudent);
         while((strForMarks = inMarks.readLine()) != null)
             listMarks.add(strForMarks);
         while((StrForSubject = inSubject.readLine()) != null)
             listSubject.add(StrForSubject);
-        arrayFileStudent = listStudent.toArray(new String[0]);
+        //Добавляем все в массивы
+        String[] arrayFileStudent = listStudent.toArray(new String[0]);
+        String[] arrayFileSubject = listSubject.toArray(new String[0]);
         arrayFileMarks = listMarks.toArray(new String[0]);
-        arrayFileSubject = listSubject.toArray(new String[0]);
+        //Массивы стрингов для каждого слова с файла
         String[] splitArrayFileStudent;
         String[] splitArrayFileMarks;
         String[] splitArrayFileSubject;
+        //List куда добавляем имена
         List<String> fullNames = new ArrayList<>(); fullNames.add("");
+        //Объявляем HashMaps
         nameAndNumber = new HashMap<>();
         subjectAndNumber = new HashMap<>();
         nameAndSubject1 = new HashMap<>();
         nameAndSubject2 = new HashMap<>();
-
+        //Получаем имена
         for(int i = 0; i < arrayFileStudent.length; i++){
             splitArrayFileStudent = arrayFileStudent[i].split("\\s+");
             String surname = splitArrayFileStudent[1];
@@ -58,14 +68,14 @@ public class Main {
             nameAndNumber.put(i + 1, finalName);
             fullNames.add(finalName);
         }
-
+        //Получаем предметы
         for(int i = 0; i < arrayFileSubject.length; i++){
             splitArrayFileSubject = arrayFileSubject[i].split("\\s+");
             int count = i + 1;
             String str = splitArrayFileSubject[1];
             subjectAndNumber.put(count, str);
         }
-
+        //Обрабытваем оценки
         for(int i = 0; i < arrayFileMarks.length; i++){
             splitArrayFileMarks = arrayFileMarks[i].split("\\s+");
             String str = splitArrayFileMarks[0]+""+splitArrayFileMarks[1];
@@ -74,7 +84,7 @@ public class Main {
             nameAndSubject1.put(Integer.parseInt(str), Integer.parseInt(answer));
             nameAndSubject2.put(count, Integer.parseInt(str));
         }
-
+        //Объявляем столбы
         List<String> row1 = Arrays.asList("Rus",
                 determineMarks(fullNames.get(1), subjectAndNumber.get(1)),
                 determineMarks(fullNames.get(2), subjectAndNumber.get(1)),
@@ -90,26 +100,28 @@ public class Main {
                 determineMarks(fullNames.get(5), subjectAndNumber.get(2))
         );
         List<String> row3 = Arrays.asList("Phy",
-                determineMarks(fullNames.get(1), subjectAndNumber.get(2)),
-                determineMarks(fullNames.get(2), subjectAndNumber.get(2)),
-                determineMarks(fullNames.get(3), subjectAndNumber.get(2)),
-                determineMarks(fullNames.get(4), subjectAndNumber.get(2)),
-                determineMarks(fullNames.get(5), subjectAndNumber.get(2))
+                determineMarks(fullNames.get(1), subjectAndNumber.get(3)),
+                determineMarks(fullNames.get(2), subjectAndNumber.get(3)),
+                determineMarks(fullNames.get(3), subjectAndNumber.get(3)),
+                determineMarks(fullNames.get(4), subjectAndNumber.get(3)),
+                determineMarks(fullNames.get(5), subjectAndNumber.get(3))
         );
         List<String> row4 = Arrays.asList("Lit",
-                determineMarks(fullNames.get(1), subjectAndNumber.get(2)),
-                determineMarks(fullNames.get(2), subjectAndNumber.get(2)),
-                determineMarks(fullNames.get(3), subjectAndNumber.get(2)),
-                determineMarks(fullNames.get(4), subjectAndNumber.get(2)),
-                determineMarks(fullNames.get(5), subjectAndNumber.get(2))
+                determineMarks(fullNames.get(1), subjectAndNumber.get(4)),
+                determineMarks(fullNames.get(2), subjectAndNumber.get(4)),
+                determineMarks(fullNames.get(3), subjectAndNumber.get(4)),
+                determineMarks(fullNames.get(4), subjectAndNumber.get(4)),
+                determineMarks(fullNames.get(5), subjectAndNumber.get(4))
         );
+        //Определяем таблицу
         table = Arrays.asList(fullNames, row1,row2, row3, row4);
-
-
+        //Установка отсупа
         int spacing = 3;
+        //Build таблицы
         printTable(spacing);
-        determineMarks(fullNames.get(1), subjectAndNumber.get(1));
+
     }
+    //Функция получения оценок
     public static String determineMarks(String numOfMan, String numOfSubject){
         final int[] interimName = new int[1];
         final int[] interimSubject = new int[1];
@@ -133,14 +145,14 @@ public class Main {
         }
        return String.valueOf(answer[0]);
     }
+    //Поиск первой буквы
     static char findFirstLetter(String str) {
         for (int i = 0; i < str.length(); i++)
             if (Character.isUpperCase(str.charAt(i)))
                 return str.charAt(i);
         return 0;
     }
-
-
+    //Функция билда
     private static void printTable(int spacing) {
         List<Integer> maxLengths = findMaxLengths();
         StringBuilder sb = new StringBuilder();
@@ -157,7 +169,7 @@ public class Main {
 
         System.out.println(sb);
     }
-
+    //Поиск максисальной длины
     private static List<Integer> findMaxLengths() {
         List<Integer> maxLengths = new ArrayList<>();
         for (List<String> row : table) {
@@ -171,5 +183,4 @@ public class Main {
         }
         return maxLengths;
     }
-
 }
